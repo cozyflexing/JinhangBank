f75422c533661a70dbf3922729acca40417385c3
Alen Iriskic <cozyflexing@gmail.com>
2023-02-23 17:17:42 +0100

    test

--- authenticationTest.py
+++ authenticationTest.py
@@ -1,51 +1,62 @@
-from time import sleep
-import sys
-from mfrc522 import SimpleMFRC522
-
-reader = SimpleMFRC522()
 users = []
 
 
 class User:
-    def __init__(self, accountNumber, cardNumber, pinCode, balance):
+    def __init__(self, accountNumber, cardNumber, pinCode, balance, tries):
         self.accountNumber = accountNumber
-        self.cards = [cardNumber]
+        self.cards = cardNumber
         self.pinCode = pinCode
         self.balance = balance
-
-    def printBalance(self):
-        print(f"Balance: {self.balance}")
-
-    def withdraw(self, amount):
-        self.balance -= amount
-
-    def add(self, amount):
-        self.balance += amount
-
-    def addCards(self):
-        try:
-            print("Hold card near reader. ")
-            id, text = reader.read()
-            print("ID: %s\nText: %s" % (id, text))
-            self.cards.append(id)
-            self.pinCode = text
-        except KeyboardInterrupt:
-            GPIO.cleanup()
-            raise
+        self.tries = tries
 
 
-def authentication(accountNumber, pinCode, user=[]):
+alen = User("NL00ABNA0000", [10101010, 20202020], "0000", 100, 0)
+armin = User("NL00ABNA0001", [30303030, 40404040], "1111", 100, 0)
+
+users.append(alen)
+users.append(armin)
+
+
+def printBalance(self):
+    print(f"Balance: {self.balance}")
+
+
+def withdraw(self, amount):
+    self.balance -= amount
+
+
+def add(self, amount):
+    self.balance += amount
+
+
+def pinCodeCheck(userPinCode, givenPinCode):
+    if userPinCode == givenPinCode:
+        return True
+    else:
+        return False
+
+
+def authentication(givenCard, users=[]):
     for user in users:
-        if user.accountNumber == accountNumber:
-            if user.pinCode == pinCode:
-                print("Authentication successful.")
-            else:
-                print("Authentication failed, incorrect pin code.")
-        else:
-            print("Authentication failed, user does not exist.")
+        if givenCard in user.cards:
+            while user.tries < 3:
+                pinCode = input("Pin code please: ")
+                if pinCodeCheck(user.pinCode, pinCode):
+                    user.tries = 0
+                    return print("Authentication Successful")
+                else:
+                    user.tries += 1
+                    if 3 - user.tries > 1:
+                        print(f"Authentication Failed. {3-user.tries} tries remaining.")
+                    else:
+                        return print(
+                            f"Authentication Failed. {3-user.tries} try remaining."
+                        )
+
+            if user.tries == 3:
+                return print("Your cards have been blocked, too many failed attempts.")
 
 
 def login():
-    accountNumber = input("Accountnumber please: ")
-    pinCode = input("Pin code please: ")
-    authentication(accountNumber, pinCode, users)
+    givenCard = input("cardNumber please: ")
+    authentication(givenCard, users)
