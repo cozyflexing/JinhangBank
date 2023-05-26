c11207ae1b3bb0aafd805d13fe51bbb2ad8f3298
Alen Iriskic <cozyflexing@gmail.com>
2023-05-26 15:23:57 +0200

    Biljetten toegevoegd

--- test.py
+++ test.py
@@ -1,10 +1,32 @@
-import serial
-import time
+input = 900
+options = {
+    "50": [{"value": 50}, {"amount": 6}],
+    "20": [{"value": 20}, {"amount": 6}],
+    "10": [{"value": 10}, {"amount": 6}],
+}
+bills = []
 
-ser = serial.Serial("/dev/serial0", 9600)  # open the serial port at 9600 baud
-
-while True:
-    if ser.in_waiting > 0:
-        line = ser.readline().decode("utf-8").rstrip()  # read a '\n' terminated line
-        print(line)
-    time.sleep(0.1)
+if input % 10 != 0:
+    print("Invalid input. Please enter an amount that is a multiple of 10.")
+else:
+    while input != 0:
+        if input > 480:
+            print("Please take a lesser amount")
+            break
+        else:
+            if input >= options["50"][0]["value"] and options["50"][1]["amount"] > 0:
+                input -= options["50"][0]["value"]
+                bills.append(options["50"][0]["value"])
+                options["50"][1]["amount"] -= 1
+            elif input >= options["20"][0]["value"] and options["20"][1]["amount"] > 0:
+                input -= options["20"][0]["value"]
+                bills.append(options["20"][0]["value"])
+                options["20"][1]["amount"] -= 1
+            elif input >= options["10"][0]["value"] and options["10"][1]["amount"] > 0:
+                input -= options["10"][0]["value"]
+                bills.append(options["10"][0]["value"])
+                options["10"][1]["amount"] -= 1
+            else:
+                print("Not enough bills for requested amount.")
+                break
+    print(f"Dispensed bills: {bills}")
