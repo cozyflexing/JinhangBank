cecbf5d22ff4600eb5efdf50c7c37f07781c8157
Alen Iriskic <cozyflexing@gmail.com>
2023-05-30 11:34:12 +0200

    hahdjhasbgdiu

--- test.py
+++ test.py
@@ -11,18 +11,26 @@ def get_ack_from_arduino(arduino):
     print(f"Received from Arduino: {ack}")
 
 
-arduino_uno = serial.Serial("/dev/ttyACM0", 9600)  # Update with your Mega's device name
-arduino_mega = serial.Serial("/dev/ttyACM1", 9600)  # Update with your Uno's device name
+def get_button_press_from_mega(arduino):
+    while True:
+        if arduino.in_waiting > 0:
+            button_press = (
+                arduino.readline().decode().strip()
+            )  # Strip to remove trailing newline
+            print(f"Button {button_press} was pressed on the Arduino Mega")
+
+
+arduino_uno = serial.Serial("/dev/ttyACM0", 9600)  # Update with your Uno's device name
+arduino_mega = serial.Serial(
+    "/dev/ttyACM1", 9600
+)  # Update with your Mega's device name
 
 time.sleep(2)  # Waiting for the Arduino to initialize
 
-send_to_arduino(arduino_mega, "Hello, Mega!")
-get_ack_from_arduino(arduino_mega)
+get_button_press_from_mega(arduino_mega)
 
 send_to_arduino(arduino_uno, "Hello, Uno!")
 get_ack_from_arduino(arduino_uno)
 
 arduino_mega.close()
 arduino_uno.close()
-
-sudo usermod -a -G dialout $USER
