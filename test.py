c13726041ac6903fbced9cb2a06f0b1e049a08fb
Alen Iriskic <cozyflexing@gmail.com>
2023-05-30 11:00:49 +0200

    sdsafdasfda

--- test.py
+++ test.py
@@ -6,6 +6,11 @@ def send_to_arduino(arduino, message):
     arduino.write(message.encode())
 
 
+def get_ack_from_arduino(arduino):
+    ack = arduino.readline().decode().strip()  # Strip to remove trailing newline
+    print(f"Received from Arduino: {ack}")
+
+
 arduino_mega = serial.Serial(
     "/dev/ttyACM0", 9600
 )  # Update with your Mega's device name
@@ -14,7 +19,10 @@ arduino_uno = serial.Serial("/dev/ttyACM1", 9600)  # Update with your Uno's devi
 time.sleep(2)  # Waiting for the Arduino to initialize
 
 send_to_arduino(arduino_mega, "Hello, Mega!")
+get_ack_from_arduino(arduino_mega)
+
 send_to_arduino(arduino_uno, "Hello, Uno!")
+get_ack_from_arduino(arduino_uno)
 
 arduino_mega.close()
 arduino_uno.close()
