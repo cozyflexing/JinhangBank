a4db05946ec1bb5bf483da38f5ea64c85eccc12f
Cozyflexing <cozyflexing@gmail.com>
2023-05-30 10:27:32 +0200

    d

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import serial
 
 bills = [10, 20, 30]
 # Start serial communication
-ser = serial.Serial("/dev/serial0", 9600)  # check your COM port and baud rate
+ser = serial.Serial("/dev/ACM0", 9600)  # check your COM port and baud rate
 
-ser.write(str(bills).encode())
+ser.write("1")
