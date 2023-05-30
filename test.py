2e9ee05d9374658613dce11dc7a54f9ea0c819b3
Alen Iriskic <cozyflexing@gmail.com>
2023-05-30 10:54:48 +0200

    pf

--- test.py
+++ test.py
@@ -1,22 +1,20 @@
 import serial
 import time
 
-ser = serial.Serial(
-    port="/dev/serial0",  # Change this as per your port name
-    baudrate=9600,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1,
-)
 
-# give the serial connection a second to settle
-time.sleep(1)
+def send_to_arduino(arduino, message):
+    arduino.write(message.encode())
 
-try:
-    ser.write(b"1")
-    print("Sent '1' to Arduino")
-except Exception as e:
-    print("Error occurred: ", e)
-finally:
-    ser.close()
+
+arduino_mega = serial.Serial(
+    "/dev/ttyACM0", 9600
+)  # Update with your Mega's device name
+arduino_uno = serial.Serial("/dev/ttyACM1", 9600)  # Update with your Uno's device name
+
+time.sleep(2)  # Waiting for the Arduino to initialize
+
+send_to_arduino(arduino_mega, "Hello, Mega!")
+send_to_arduino(arduino_uno, "Hello, Uno!")
+
+arduino_mega.close()
+arduino_uno.close()
