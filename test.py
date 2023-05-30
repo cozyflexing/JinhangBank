cde56a9c1db90e40ff32d665b218b7acb8a195d6
Cozyflexing <cozyflexing@gmail.com>
2023-05-30 11:21:09 +0200

    sfdfsdagsfd

--- test.py
+++ test.py
@@ -24,23 +24,3 @@ get_ack_from_arduino(arduino_uno)
 
 arduino_mega.close()
 arduino_uno.close()
-
-Traceback (most recent call last):
-  File "/usr/lib/python3/dist-packages/serial/serialposix.py", line 322, in open
-    self.fd = os.open(self.portstr, os.O_RDWR | os.O_NOCTTY | os.O_NONBLOCK)
-PermissionError: [Errno 13] Permission denied: '/dev/serial0'
-
-During handling of the above exception, another exception occurred:
-
-Traceback (most recent call last):
-  File "/home/pi/Desktop/JinhangBank/Website/App.py", line 10, in <module>
-    app = create_app()
-  File "/home/pi/Desktop/JinhangBank/Website/views/__init__.py", line 23, in create_app
-    from .bePatient import bePatient_bp
-  File "/home/pi/Desktop/JinhangBank/Website/views/bePatient.py", line 8, in <module>
-    ser = serial.Serial("/dev/serial0", 9600)  # check your COM port and baud rate
-  File "/usr/lib/python3/dist-packages/serial/serialutil.py", line 244, in __init__
-    self.open()
-  File "/usr/lib/python3/dist-packages/serial/serialposix.py", line 325, in open
-    raise SerialException(msg.errno, "could not open port {}: {}".format(self._port, msg))
-serial.serialutil.SerialException: [Errno 13] could not open port /dev/serial0: [Errno 13] Permission denied: '/dev/serial0'
